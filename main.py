5602d4323f64b2aed29ab4fb6b9556b0ebc2de94
HarumaIto <itoharm@gmail.com>
2025-07-20 23:06:51 +0900

    fix: main

--- main.py
+++ main.py
@@ -56,4 +56,5 @@ def post_congestion():
         return jsonify({"detail": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+        port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
